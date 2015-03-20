58cffc5de05514f75c69acc02f222e0b7d333e2c
Lazaros Inepologlou <li@tcpartners.fr>
2015-03-20 10:36:58 +0100

    ...

--- _.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('oxy/_.php');
-require('mmm/_.php');
-
-Oxygen::SetWindowScopingEnabled(false);
-Oxygen::SetDatabaseManaged('localhost','mmx','root','');
-Oxygen::Init();
--- mmm/_.php
+++
@@ -1,5 +0,0 @@
-<?php
-require('mmm.php');
-Oxygen::AddCodeFolder('mmm/skn');
-Oxygen::AddCodeFolder('mmm/src');
-Oxygen::AddDatabaseUpgradeFile('mmm/mmm_upgrade.php');
